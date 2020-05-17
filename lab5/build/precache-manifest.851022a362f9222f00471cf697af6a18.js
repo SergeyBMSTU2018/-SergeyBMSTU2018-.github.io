6652320bb2b5ffd5020ac31d313649203ab2992a
Sergey Alekhin <alekhin1991@gmail.com>
2020-05-17 22:53:56 +0300

    Add lab5

---
+++ lab5/build/precache-manifest.851022a362f9222f00471cf697af6a18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8addbe2a2a003e86b9ae09c9f3f9a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8671d3e5d590a6660dd9",
+    "url": "/static/css/main.450a5ba6.chunk.css"
+  },
+  {
+    "revision": "dbaa989c1b7d17275457",
+    "url": "/static/js/2.3a1d53f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3a1d53f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8671d3e5d590a6660dd9",
+    "url": "/static/js/main.2ecf7646.chunk.js"
+  },
+  {
+    "revision": "9227409595cd0e4bea71",
+    "url": "/static/js/runtime-main.14f13e0a.js"
+  }
+]);
